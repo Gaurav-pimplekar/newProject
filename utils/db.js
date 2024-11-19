6d3f725f759855176738bcaf316756a2422b179c
Gaurav-pimplekar <gpimpleakr@gmail.com>
2024-11-19 19:23:31 +0530

    data update

--- utils/db.js
+++ utils/db.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose"
 const connectDB = async ()=>{
   try {
 
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO);
     console.log("database connect successfully");
     
   } catch (error) {
